36d8205edc321eecdb4069c0b1087cbf9c5f0708
lombardo-luca <luca.lombardo.dev@gmail.com>
2025-09-27 21:39:45 +0200

    added icon

--- lepran.py
+++ lepran.py
@@ -3,13 +3,18 @@ LePrAn - Letterboxd Profile Analyzer
 Main application entry point.
 """
 import sys
-from PyQt6 import QtWidgets
+from PyQt6 import QtWidgets, QtGui
 from src.main_window import MainWindow
 
 
 def main():
     """Main application entry point."""
     app = QtWidgets.QApplication(sys.argv)
+    
+    # Set application-wide window icon
+    app_icon = QtGui.QIcon("gfx/icon.png")
+    app.setWindowIcon(app_icon)
+    
     window = MainWindow()
     window.show()
     app.exec()
